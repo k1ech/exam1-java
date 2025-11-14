9ff8b4cf9002a3c36162b055b72c8d4f3a27ddca
KRZYSZTOF KIECH <krzysztof.kiech77@gmail.com>
2025-11-14 19:24:05 +0100

    work on SequenceNumbers

---
+++ src/main/java/zadanie2/SequenceNumbers.java
@@ -0,0 +1,23 @@
+package zadanie2;
+
+public class SequenceNumbers {
+
+    public static void main(String[] args) {
+
+        int[] sequence = {2, 4, 8, 16, 32};
+        int[] sequence1 = {2, 4, 6, 8, 10};
+        int[] sequence2 = {10, 8, 2, 5, 6};
+
+    }
+
+
+    public static String getSequenceName(int[] sequence) {
+
+        if (sequence.length < 2) {
+            return "Ciąg jest za krótki";
+        }
+
+
+    }
+
+}
